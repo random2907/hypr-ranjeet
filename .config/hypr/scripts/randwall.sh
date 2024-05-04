82325abd328bc8048c74330df124f606ef2df956
random2907 <yranjeetyadav1234567890@gmail.com>
2024-05-04 15:28:31 +0530

    update

--- .config/hypr/scripts/randwall.sh
+++ .config/hypr/scripts/randwall.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
-
+if [ -z "$1" ]; then
 BG="$(find $wallpaper_path | shuf -n 1)"
+else
+	BG="$1"
+fi
 
-trans_type="grow"
+trans_type="wipe"
 # trans_type="center"
 # Name of the program to check
 PROGRAM="swww-daemon"
 
 # Check if the program is running
 if pgrep "$PROGRAM" >/dev/null; then
-	swww img "$BG" --transition-type $trans_type
-	wal -i $BG -s
+	swww img "$BG" --transition-type none
+	wal -i $BG -n -s
 	killall dunst
 	killall -USR2 cava
-	killall -SIGUSR2 waybar
 else
-	swww init
+	swww-daemon &
 fi
