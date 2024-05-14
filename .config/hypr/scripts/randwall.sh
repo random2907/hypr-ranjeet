79b40c43b3e7eff988c66d3b19e42a5f04ae7858
random2907 <yarnjeetyadav1234567890@gmail.com>
2024-05-14 19:12:02 +0530

    update

--- .config/hypr/scripts/randwall.sh
+++ .config/hypr/scripts/randwall.sh
@@ -5,15 +5,15 @@ else
 	BG="$1"
 fi
 
-trans_type="wipe"
+trans_type="simple"
 # trans_type="center"
 # Name of the program to check
 PROGRAM="swww-daemon"
 
 # Check if the program is running
 if pgrep "$PROGRAM" >/dev/null; then
-	swww img "$BG" --transition-type none
-	wal -i $BG -n -s
+	swww img "$BG" --transition-type "$trans_type" --transition-step 10
+	wal -i "$BG" -n -s
 	killall dunst
 	killall -USR2 cava
 else
